0b3583e606cc231b9c809ecf2276f2a8f7fb3727
Eugene Veselov <xh2ul38p2urmqkl4smyke506ce5q04@bots.bitbucket.org>
2026-03-18 14:15:26 -0700

    Timeout for token popup hiding increaded to two minutes.

--- src/components/InsightsPanel.vue
+++ src/components/InsightsPanel.vue
@@ -151,7 +151,7 @@ function showRequestToast(before: TokenStats, after: TokenStats) {
   if (delta.llmIn === 0 && delta.llmOut === 0 && delta.embedIn === 0) return
   if (toastTimer) clearTimeout(toastTimer)
   toast.value = delta
-  toastTimer = setTimeout(() => { toast.value = null }, 12000)
+  toastTimer = setTimeout(() => { toast.value = null }, 120 * 1000)
 }
 
 onMounted(async () => {
